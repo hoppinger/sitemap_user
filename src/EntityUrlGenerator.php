7ef1d442e4c668c6040571881a44af6ccc5f34bf
Ashwini Mone <moneashwini26@gmail.com>
2020-01-09 17:01:47 +0100

    first commit

---
+++ src/EntityUrlGenerator.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Drupal\sitemap_user;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\simple_sitemap\EntityHelper;
+use Drupal\simple_sitemap\Logger;
+use Drupal\simple_sitemap\Plugin\simple_sitemap\UrlGenerator\EntityUrlGenerator as BaseGenerator;
+use Drupal\simple_sitemap\Plugin\simple_sitemap\UrlGenerator\UrlGeneratorManager;
+use Drupal\simple_sitemap\Simplesitemap;
+use Drupal\simple_sitemap\SitemapGenerator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+
+class EntityUrlGenerator extends BaseGenerator {
+  /**
+   * EntityUrlGenerator constructor.
+   * 
+   * @param array $configuration
+   * @param string $plugin_id
+   * @param mixed $plugin_definition
+   * @param \Drupal\simple_sitemap\Simplesitemap $generator
+   * @param \Drupal\simple_sitemap\SitemapGenerator $sitemap_generator
+   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   * @param \Drupal\simple_sitemap\Logger $logger
+   * @param \Drupal\simple_sitemap\EntityHelper $entityHelper
+   * @param \Drupal\simple_sitemap\Plugin\simple_sitemap\UrlGenerator\UrlGeneratorManager $url_generator_manager
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    Simplesitemap $generator,
+    SitemapGenerator $sitemap_generator,
+    LanguageManagerInterface $language_manager,
+    EntityTypeManagerInterface $entity_type_manager,
+    Logger $logger,
+    EntityHelper $entityHelper,
+    UrlGeneratorManager $url_generator_manager,
+    $api_key_name
+  ) {
+    parent::__construct(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $generator,
+      $sitemap_generator,
+      $language_manager,
+      $entity_type_manager,
+      $logger,
+      $entityHelper,
+      $url_generator_manager
+    );
+    
+    $api_key_storage = $this->entityTypeManager->getStorage('api_key');
+    $api_key = $api_key_storage->load($api_key_name);
+
+    if (!$api_key) {
+      return;
+    }
+
+    $user_storage = $this->entityTypeManager->getStorage('user');
+    $users = $user_storage->loadByProperties(array('uuid' => $api_key->user_uuid));
+
+    if (empty($users)) {
+      return;
+    }
+
+    $this->anonUser = reset($users);
+  }
+
+  public static function create(
+    ContainerInterface $container,
+    array $configuration,
+    $plugin_id,
+    $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('simple_sitemap.generator'),
+      $container->get('simple_sitemap.sitemap_generator'),
+      $container->get('language_manager'),
+      $container->get('entity_type.manager'),
+      $container->get('simple_sitemap.logger'),
+      $container->get('simple_sitemap.entity_helper'),
+      $container->get('plugin.manager.simple_sitemap.url_generator'),
+      $container->getParameter('sitemap_user.api_key')
+    );
+  }
+}
